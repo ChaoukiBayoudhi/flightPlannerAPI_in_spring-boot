38c508f7c1318f3d85ba4411b7ea611cb75be535
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2023-04-05 18:21:41 +0100

    second commit

--- src/main/java/tn/esb/lmad/flightPlannerAPI/Domains/Aircraft.java
+++ src/main/java/tn/esb/lmad/flightPlannerAPI/Domains/Aircraft.java
@@ -1,13 +1,30 @@
 package tn.esb.lmad.flightPlannerAPI.Domains;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import lombok.*;
+import tn.esb.lmad.flightPlannerAPI.Enumerations.AircraftModel;
 
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
    // JPA annotation to make this object ready for storage in a JPA-based data store
 //the database is a relational one
+@Entity
 public class Aircraft {
     @Id // JPA annotation to specify the primary key of an entity
     private String code;
-    private String model;
+    @Enumerated(EnumType.STRING)
+    private AircraftModel model;
+    private int maxPassengerCapacity;
+    private  int range;
+    private BigDecimal fuelConsumption;
+
 }
--- src/main/java/tn/esb/lmad/flightPlannerAPI/Domains/Airline.java
+++ src/main/java/tn/esb/lmad/flightPlannerAPI/Domains/Airline.java
@@ -1,6 +1,6 @@
 package tn.esb.lmad.flightPlannerAPI.Domains;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -9,7 +9,26 @@ import lombok.*;
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "airlines")
 @Entity
+//in SQL the annotation is translated into
+//CREATE TABLE airlines (code INTEGER PRIMARY KEY AUTO_INCREMENT, name VARCHAR(70) NOT NULL UNIQUE);
+
 public class Airline {
-    private
+    //primary key
+    @Id
+    //auto increment starts from 1 and increments by 1
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer code;
+    @NonNull // JPA annotation to specify that a property is not nullable
+    //@Column annotation to specify the column name, length ...
+    @Column(unique = true,length = 70,name="airline_name")
+    private String name;
+    private String contactInformations;
+    private String headQuarters;
+    @Lob // JPA annotation to specify that a property will be
+    // persisted as a large object to a database-supported
+    // large object type
+    private byte[] logo;
+    private int fleetSize;
 }
